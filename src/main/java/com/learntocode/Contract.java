40344315837d14914fbabbf06d856d7b5709a218
jmejia1920 <jonny86493@gmail.com>
2023-05-18 15:03:01 -0400

    Added constructor, setter and getters to the contract class.

--- src/main/java/com/learntocode/Contract.java
+++ src/main/java/com/learntocode/Contract.java
@@ -1,4 +1,67 @@
 package com.learntocode;
 
 public class Contract {
+    private String dateOfContract;
+    private String customerName;
+    private String email;
+    private boolean vehicleSold;
+    private double totalPrice;
+    private int monthlyPayment;
+
+    public Contract(String dateOfContract, String customerName, String email, boolean vehicleSold, double totalPrice, int monthlyPayment) {
+        this.dateOfContract = dateOfContract;
+        this.customerName = customerName;
+        this.email = email;
+        this.vehicleSold = false;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public String getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(String dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(boolean vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public void setMonthlyPayment(int monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
 }
